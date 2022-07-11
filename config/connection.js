b7a8073d07721be1dbcab634a3a26e742b784b1b
Kishan Shah <kishanshah@kishans-air.myfiosgateway.com>
2022-07-11 17:37:35 -0400

    Completed models

--- config/connection.js
+++ config/connection.js
@@ -1,6 +1,6 @@
 const { connect, connection } = require('mongoose');
 
-connect('mongodb://localhost/developersApplications', {
+connect('mongodb://localhost/reactionsAndThoughts', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
